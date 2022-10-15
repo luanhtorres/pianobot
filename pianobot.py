0955172b2715f5c7ae411f709fa9e3258f796e3c
Luan Torres <100809026+luanhtorres@users.noreply.github.com>
2022-10-15 13:04:12 -0300

    Add files via upload

---
+++ pianobot.py
@@ -0,0 +1,57 @@
+import time
+import pyautogui
+import numpy
+import keyboard
+
+## link = "https://lagged.com.br/jogo/1461/"
+
+while keyboard.is_pressed('q') == False:
+        
+
+        if pyautogui.pixel(736,250) [0] == 74:
+            pyautogui.click(x =736, y =250)
+
+        if pyautogui.pixel(906,250) [0] == 74:
+            pyautogui.click(x = 906,y = 250)
+
+        if pyautogui.pixel(1068,250) [0] == 74:
+            pyautogui.click(x = 1068,y =250)
+
+        if pyautogui.pixel(1232,250) [0] == 74:
+            pyautogui.click(x = 1232,y =250)
+
+        if pyautogui.pixel(736,420) [0] == 74:
+            pyautogui.click(x = 736,y =420)
+
+        if pyautogui.pixel(906,420) [0] == 74:
+            pyautogui.click(x = 906,y =420)
+
+        if pyautogui.pixel(1068,420) [0] == 74:
+            pyautogui.click(x = 1068,y =420)
+
+        if pyautogui.pixel(1232,420) [0] == 74:
+            pyautogui.click(x = 1232,y =420)
+
+        if pyautogui.pixel(736,580) [0] == 74:
+            pyautogui.click(x = 736,y =580)
+
+        if pyautogui.pixel(906,580) [0] == 74:
+            pyautogui.click(x = 906,y =580)
+
+        if pyautogui.pixel(1068,580) [0] == 74:
+            pyautogui.click(x = 1068,y =580)
+
+        if pyautogui.pixel(1232,580) [0] == 74:
+            pyautogui.click(x = 1232,y =580)
+
+        if pyautogui.pixel(736,750) [0] == 74:
+            pyautogui.click(x = 736,y =750)
+
+        if pyautogui.pixel(906,750) [0] == 74:
+            pyautogui.click(x = 906,y =750)
+
+        if pyautogui.pixel(1068,750) [0] == 74:
+            pyautogui.click(x = 1068,y =750)
+
+        if pyautogui.pixel(1232,750) [0] == 74:
+           pyautogui.click(x = 1232,y =750)
